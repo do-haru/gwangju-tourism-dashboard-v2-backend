635c21d8db3b559ce36abf1a4d5e9efb961e0235
do-haru <doharu05@naver.com>
2026-03-19 14:55:50 +0900

    feat: Spring backend 초기 프로젝트 생성

---
+++ src/main/java/com/doharu/gwangju_tourism_dashboard_v2/GwangjuTourismDashboardV2Application.java
@@ -0,0 +1,13 @@
+package com.doharu.gwangju_tourism_dashboard_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwangjuTourismDashboardV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwangjuTourismDashboardV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/doharu/gwangju_tourism_dashboard_v2/GwangjuTourismDashboardV2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doharu.gwangju_tourism_dashboard_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwangjuTourismDashboardV2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
