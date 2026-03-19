e099a11c2f00130e7b63043f261ae70f322fd5f5
do-haru <doharu05@naver.com>
2026-03-19 17:23:02 +0900

    feat: 방문자 에측 알고리즘 적용

--- src/main/java/com/doharu/gwangju_tourism_dashboard_v2/service/PredictionService.java
+++ src/main/java/com/doharu/gwangju_tourism_dashboard_v2/service/PredictionService.java
@@ -22,17 +22,38 @@ public class PredictionService {
         YearMonth yearMonth = YearMonth.of(yearInt, monthInt);
         int daysInMonth = yearMonth.lengthOfMonth();
 
-        System.out.println("days: " + daysInMonth);
-        System.out.println("year: " + year);
-        System.out.println("month: " + month);
+        List<Integer> data = ((List<?>) request.get("series"))
+                .stream()
+                .map(value -> (Integer) value)
+                .toList();
+        int n = Math.min(14, data.size()); // 최근 14개만 사용
+        List<Integer> recent = data.subList(data.size() - n, data.size());
+        double sumX = 0;
+        double sumY = 0;
+        double sumXY = 0;
+        double sumXX = 0;
+
+        for (int i = 0; i < n; i++) {
+            double x = i;               // 시간 (0,1,2,...)
+            double y = recent.get(i);  // 방문자 수
+
+            sumX += x;
+            sumY += y;
+            sumXY += x * y;
+            sumXX += x * x;
+        }
+
+        double slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
+        double intercept = (sumY - slope * sumX) / n;
 
         List<Integer> predictions = new ArrayList<>();
 
         for (int i = 0; i < daysInMonth; i++) {
-            // 간단한 더미 데이터 (랜덤 or 증가형)
-            int value = 150 + (int)(Math.random() * 50);
-            predictions.add(value);
+            double x = n + i; // 미래 시간
+            int predicted = (int) (slope * x + intercept);
+            predictions.add(predicted);
         }
+
 
         return Map.of(
                 "predictions", predictions,
